2aa8c22c790d6cad9e697186457daa49b0b08e43
yu3756@naver.com <yu3756@naver.com>
2023-01-30 19:44:50 +0900

    4th

--- flask_app.py
+++ flask_app.py
@@ -4,8 +4,8 @@ from flask import *
 from pytube import YouTube
 from moviepy.editor import *
 # from selenium import webdriver
-from tkinter import filedialog
-from tkinter import *
+# from tkinter import filedialog
+# from tkinter import *
 # from pytube.cli import on_progress
 # from tqdm import tqdm
 # from requests import ConnectionError
@@ -25,8 +25,8 @@ import requests
 import keyboard
 # import pyautogui
 
-import matplotlib
-matplotlib.use('TkAgg')
+# import matplotlib
+# matplotlib.use('TkAgg')
 
 app = Flask(__name__)
 #app.config.from_object(__name__)
@@ -127,127 +127,127 @@ def mp3_check():
 
 
 
-@app.route('/down', methods=['GET'])
-def down():
-	# client -> server 데이터 전송은 form 으로 받을 수 있다
-	# 선택한 화질
-	quality = request.args.get("quality") 
+# @app.route('/down', methods=['GET'])
+# def down():
+# 	# client -> server 데이터 전송은 form 으로 받을 수 있다
+# 	# 선택한 화질
+# 	quality = request.args.get("quality") 
 
 
 
-	# 파일 저장 경로창 정하기 위한 flag 변수
-	if "mp3" in quality:
-		mp3_flag = 1
-	else:
-		mp3_flag = 0
+# 	# 파일 저장 경로창 정하기 위한 flag 변수
+# 	if "mp3" in quality:
+# 		mp3_flag = 1
+# 	else:
+# 		mp3_flag = 0
 
-	# 파일 저장 경로
+# 	# 파일 저장 경로
 	
-	dir = save(mp3_flag)
+# 	dir = save(mp3_flag)
 	
 	
-	# 파일저장 창을 닫았으면
-	if dir == '':
-		return render_template('cancel.html', video_info=video_info)
+# 	# 파일저장 창을 닫았으면
+# 	if dir == '':
+# 		return render_template('cancel.html', video_info=video_info)
 	
-	# 파일명 정하기 '/' 기준으로 자름
-	dir_split = dir.split('/')
+# 	# 파일명 정하기 '/' 기준으로 자름
+# 	dir_split = dir.split('/')
 
-	# 자른 리스트 i번째 까지 합침
-	file_dir = '/'.join(dir_split[:-1])
+# 	# 자른 리스트 i번째 까지 합침
+# 	file_dir = '/'.join(dir_split[:-1])
 
-	# 디렉토리를 구하고 /를 붙여놔야 파일명만 구할수 있음
-	file_dir = file_dir + '/'
-	file_name = dir.replace(file_dir, '')
+# 	# 디렉토리를 구하고 /를 붙여놔야 파일명만 구할수 있음
+# 	file_dir = file_dir + '/'
+# 	file_name = dir.replace(file_dir, '')
 
-	# mp3 itag 구하는 로직
-	# html <option value == "mp3">
-	if "mp3" in quality:
-		for i, q in enumerate(audio_quality):
-			# value 에 붙여놓은 ".mp3" 를 quality랑 합쳐서 검사
-			if q + ".mp3" == quality:
-				tag = audio_itag[i]
+# 	# mp3 itag 구하는 로직
+# 	# html <option value == "mp3">
+# 	if "mp3" in quality:
+# 		for i, q in enumerate(audio_quality):
+# 			# value 에 붙여놓은 ".mp3" 를 quality랑 합쳐서 검사
+# 			if q + ".mp3" == quality:
+# 				tag = audio_itag[i]
 		
-		audio_path = video.streams.get_by_itag(tag)
-		audio_path = audio_path.download(output_path=file_dir, filename=file_name)
+# 		audio_path = video.streams.get_by_itag(tag)
+# 		audio_path = audio_path.download(output_path=file_dir, filename=file_name)
 		
-	# 동영상 다운로직
-	else:
-		# 선택한 quality 에 맞는 itag 를 구해옴
-		for i, q in enumerate(video_quality): 
-			if q + ".mp4" == quality:
-				tag = video_itag[i]
+# 	# 동영상 다운로직
+# 	else:
+# 		# 선택한 quality 에 맞는 itag 를 구해옴
+# 		for i, q in enumerate(video_quality): 
+# 			if q + ".mp4" == quality:
+# 				tag = video_itag[i]
 
-		# download 하려면 변수에 stream 이 있어야 된다
-		video_path = video.streams.get_by_itag(tag)
-		audio_path = video.streams.get_audio_only()
+# 		# download 하려면 변수에 stream 이 있어야 된다
+# 		video_path = video.streams.get_by_itag(tag)
+# 		audio_path = video.streams.get_audio_only()
 
         
 
-		video_path = video_path.download(output_path=file_dir, filename=file_name.replace('.mp4', '')+"_video.mp4")
-		audio_path = audio_path.download(output_path=file_dir, filename=file_name.replace('.mp4', '')+"_audio.mp3")
+# 		video_path = video_path.download(output_path=file_dir, filename=file_name.replace('.mp4', '')+"_video.mp4")
+# 		audio_path = audio_path.download(output_path=file_dir, filename=file_name.replace('.mp4', '')+"_audio.mp3")
 
-		# stream을 클립으로 저장
-		videoclip = VideoFileClip(video_path) 
-		audioclip = AudioFileClip(audio_path)
+# 		# stream을 클립으로 저장
+# 		videoclip = VideoFileClip(video_path) 
+# 		audioclip = AudioFileClip(audio_path)
 
-		videoclip.audio = audioclip
+# 		videoclip.audio = audioclip
 		
-		videoclip.write_videofile(file_name)
-		videoclip.close()
-		audioclip.close()
+# 		videoclip.write_videofile(file_name)
+# 		videoclip.close()
+# 		audioclip.close()
 	
 		
 		
-		os.remove(video_path)
-		os.remove(audio_path)
+# 		os.remove(video_path)
+# 		os.remove(audio_path)
 		
-		# 망할 이 한줄때문에 삽질 5일함.. 연속으로 같은 파일 다운받으면 오류나서
-		# video_path 개체를 제거해줘야 함
-		del video_path
-		del audio_path
+# 		# 망할 이 한줄때문에 삽질 5일함.. 연속으로 같은 파일 다운받으면 오류나서
+# 		# video_path 개체를 제거해줘야 함
+# 		del video_path
+# 		del audio_path
 
 	
-	return render_template('loading.html', video_info=video_info)
+# 	return render_template('loading.html', video_info=video_info)
 	
-def save(mp3_flag):
-	# tk(), mainloop() tk 기본 틀
-	file = Tk()
+# def save(mp3_flag):
+# 	# tk(), mainloop() tk 기본 틀
+# 	file = Tk()
 
-	# tk 기본창 숨김, 경로 선택창 최상위로
+# 	# tk 기본창 숨김, 경로 선택창 최상위로
 	
-	file.withdraw()
-	file.wm_attributes("-topmost", 1)
-	file.focus()
-	# ESC 누르면 먹통되는것 방지
+# 	file.withdraw()
+# 	file.wm_attributes("-topmost", 1)
+# 	file.focus()
+# 	# ESC 누르면 먹통되는것 방지
 	
 	
-	# mp3 검사
-	if mp3_flag:
+# 	# mp3 검사
+# 	if mp3_flag:
 		
-		file.dir = filedialog.asksaveasfilename(initialfile = video.title.replace('/','')+'.mp3', title="Save As", filetypes=[("MPEG3 files", "*.mp3"),("all files", "*")])
+# 		file.dir = filedialog.asksaveasfilename(initialfile = video.title.replace('/','')+'.mp3', title="Save As", filetypes=[("MPEG3 files", "*.mp3"),("all files", "*")])
 			
 		
-		# 파일명에 mp3가 없고 취소가 안 됐으면 확장자를 붙임
-		if ".mp3" not in file.dir and file.dir != '':
-			file.dir = file.dir + ".mp3"
+# 		# 파일명에 mp3가 없고 취소가 안 됐으면 확장자를 붙임
+# 		if ".mp3" not in file.dir and file.dir != '':
+# 			file.dir = file.dir + ".mp3"
 		
 
-	else:
-		try:
-			file.dir = filedialog.asksaveasfilename(initialfile	= video.title.replace('/', '')+'.mp4', title="Save As", filetypes=[("MPEG4 files", "*.mp4"),("all files", "*")])
+# 	else:
+# 		try:
+# 			file.dir = filedialog.asksaveasfilename(initialfile	= video.title.replace('/', '')+'.mp4', title="Save As", filetypes=[("MPEG4 files", "*.mp4"),("all files", "*")])
 			
-		except Exception:
-			return render_template('cancel.html', video_info=video_info)
-		if ".mp4" not in file.dir and file.dir != '':
-			file.dir = file.dir + ".mp4"
+# 		except Exception:
+# 			return render_template('cancel.html', video_info=video_info)
+# 		if ".mp4" not in file.dir and file.dir != '':
+# 			file.dir = file.dir + ".mp4"
 
 
 
-	file.destroy()
-	file.mainloop()
+# 	file.destroy()
+# 	file.mainloop()
 
-	return file.dir
+# 	return file.dir
 
 
 if __name__ == "__main__":
